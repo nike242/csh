b34c66b5892ecda08c478769bcca54e35dac81ca
Nikita Nazarov <nike242@mail.ru>
2021-02-07 16:33:22 +0300

    add 2 lesson tasks

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    class Program
+    {
+        #region Задание 1
+        // Назаров. Написать метод, возвращающий минимальное из трех чисел.
+        #endregion
+
+        static int Min(int a, int b, int c)
+        {
+            if (a <= b & a <= c)
+                return a;
+            else if (b <= a & b <= c)
+                return b;
+            else
+                return c;
+        }
+        static void Main(string[] args)
+        {
+            int a = 15, b = 222, c = -60;
+            Console.WriteLine(Min(a, b, c));
+        }
+    }
+}
---
+++ task4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        // Назаров
+        /* 
+           Реализовать метод проверки логина и пароля. На вход подается логин и пароль. 
+           На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). 
+           Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает. 
+           С помощью цикла do while ограничить ввод пароля тремя попытками. 
+        */
+
+        static bool CheckLogin(string user, string pass)
+        {
+            if (user == "root" & pass == "GeekBrains")
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int count_max = 3;
+            Console.WriteLine("Введите логин: ");
+            string user = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Введите пароль: ");
+                string pass = Console.ReadLine();
+                count++;
+                if (CheckLogin(user, pass))
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректные данные.\n");
+                    if (count == count_max)
+                        Console.WriteLine("Вы исчерпали попытки ввода.");
+                }
+
+            }
+            while (count < count_max);
+        }
+    }
+}
